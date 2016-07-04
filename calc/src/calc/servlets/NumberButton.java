9fa249f4529c7a40be91658f0c1b3154c6c508ae
firsachi.alex@gmail.com <firsachi.alex@gmail.com>
2016-07-04 17:51:27 +0300

    action add number to form

---
+++ calc/src/calc/servlets/NumberButton.java
@@ -0,0 +1,46 @@
+package calc.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OneButton
+ */
+@WebServlet("/NumberButton")
+public class NumberButton extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NumberButton() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		StringBuilder sb = new StringBuilder();
+		if (null!=request.getSession().getAttribute("counter")){
+			sb.append(request.getSession().getAttribute("counter"));
+		}
+		sb.append(request.getParameter("value"));
+		request.getSession().setAttribute("counter", sb.toString());
+		response.sendRedirect("/calc");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
